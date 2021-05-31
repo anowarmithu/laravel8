fb4f768862b889318114ee6538cf23ad03304bec
Anowar Office <anowarhosenmithu@gmail.com>
2021-05-31 16:59:59 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    echo "This is Home Page";
+});
+
+Route::get('/about', function () {
+    return view('about');
+})->middleware('age');
+
+Route::get('/contact', [ContactController::class, 'index']);
